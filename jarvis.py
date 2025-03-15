807072c84abb116e20a7a83d92f26cbd289a95d9
KapilBudhani <budhanikapil0015@gmail.com>
2025-03-15 12:07:45 +0530

    jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,22 +1,4 @@
-import time
-import pyttsx3
-import speech_recognition as sr
-import datetime
-import wikipedia
-import webbrowser
-import os
-import smtplib
-from selenium import webdriver
-import subprocess as sp
-import random
-import pyautogui
-import gradio as gr
-from transformers import pipeline
-import threading
-import logging
-from datetime import datetime
-import getpass
-from email.mime.text import MIMEText
+from imports import *
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
